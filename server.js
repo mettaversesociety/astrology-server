cbfca141cf037d2fd569583a26214ab3c1dd1667
Your Name <you@example.com>
2023-08-09 12:37:12 -0400

    Refactor code to improve performance and readability.

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-npm init -y
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+  const { name } = req.body;
+  res.send(`Hello, ${name}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
