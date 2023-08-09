b6bae340a95d56d9fa4e2e4f9bbdfeb96de26b53
Your Name <you@example.com>
2023-08-09 13:57:12 -0400

    aider: Serve index.html on get '/'.
    # Aider chat conversation:
    
    USER: modify the get '/' to serve index.html
    
    ASSISTANT: No changes are needed.

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.post('/', async (req, res) => {
