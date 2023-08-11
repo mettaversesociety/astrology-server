8a1b211b9e332aa0a91abdb4923f76bcac425938
Your Name <you@example.com>
2023-08-10 21:45:56 -0400

    Refactor code to improve performance and readability.

--- server.js
+++ server.js
@@ -1,83 +1,200 @@
 
+import dotenv from 'dotenv';
 import express from 'express';
-import { urlencoded, json, static as expressStatic} from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import passport from 'passport';
+import session from 'express-session';
+import './middleware/discord-auth.js';
 import cors from 'cors';
-import pkg from 'circular-natal-horoscope-js';
-import ephemeris from 'ephemeris';
-import moment from 'moment';
+
+import { connectToDatabase, ObjectId } from './db.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
+const discordPlayerData = {};
 
-app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(urlencoded({ extended: true }));
-app.use(json());
-app.use(expressStatic('public'));
+app.use(cors());
 
-app.post('/astro', async (req, res) => {
-  try {
-      let birthDateTime = req.body.birthDate + 'T' + req.body.birthTime + 'Z';
-      birthDateTime = moment(birthDateTime).isValid() ? new Date(birthDateTime) : new Date("1986-04-23T06:21:00Z");
+// Set up session and passport middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 
-      let birthLocation = req.body.birthLocation;
-      const url = `https://nominatim.openstreetmap.org/search?format=json&q=${birthLocation}`;
-      const response = await fetch(url);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
 
-      if (!response.ok) {
-        console.error(`HTTP error! status: ${response.status}`);
-        return res.status(400).send('Invalid input.');
-      } else if (!response.headers.get('content-type').startsWith('application/json')) {
-        console.error(`Invalid content type! Expected application/json but received ${response.headers.get('content-type')}`);
-        return res.status(400).send('Invalid input.');
+// Connect to the MongoDB database
+connectToDatabase()
+  .then((db) => {
+    if (!db) {
+      console.error('Failed to connect to database.');
+      process.exit(1);
+    }
+
+    console.log('Connected to MongoDB!');
+    const playerCollection = db.collection('playerRecords');
+
+    // Middleware to add player data to requests
+    app.use(async (req, res, next) => {
+      if (req.path === '/auth/discord' || req.path === '/auth/discord/callback') {
+        next();
+      } else if (!req.isAuthenticated()) {
+        console.log("NOT AUTHENTICATED!")
+        return res.redirect('/auth/discord');
+      } else if (req.isAuthenticated()) {
+        console.log("AUTHENTICATED!", req.isAuthenticated());
+        const discordUserId = req.user.id;
+
+        console.log("REQ USER ", discordPlayerData)
+
+        let player = await playerCollection.findOne({ discordUserId });
+
+        if (!player) {
+          try {
+            player = await playerCollection.insertOne({
+              _id: new ObjectId,
+              discordUserId: discordUserId,
+              currency: parseInt(0),
+              createdAt: new Date()
+            });
+          } catch (error) {
+            if (error instanceof MongoError && error.message.includes('Document failed validation')) {
+              console.log('Validation failed:', error.message);
+            } else {
+              console.log('Error inserting document:', error);
+            }
+          }
+        }
+
+        req.player = player;
+        discordPlayerData.id = player.discordUserId;
+        discordPlayerData.username = req.user.username
+        discordPlayerData.currency = player.currency;
+
+        // Pass db and playerCollection to io object
+        io.db = db;
+        io.playerCollection = playerCollection;
+
+        next();
+      }
+    });
+
+    // Serve static files from the public directory
+    // app.use(express.static(__dirname + '/public/'));
+
+    // Set up authentication routes
+    app.get('/auth/discord', passport.authenticate('discord', { scope: ['identify'] }));
+    app.get('/auth/discord/callback', passport.authenticate('discord', { failureRedirect: '/' }), (req, res) => {
+      req.session.discordUserId = req.user.id;
+      res.redirect('/home');
+    });
+
+    // Set up main route
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/public/index.html');
+    });
+
+    // Set up home route
+    app.get('/home', (req, res) => {
+      if (req.isAuthenticated()) {
+        res.sendFile(__dirname + '/public/home.html');
+        console.log("AUTHENTICATED!")
+      }
+      else {
+        res.redirect('/');
       }
 
-      const data = await response.json();
-      if (data.length === 0) {
-        console.error(`No location found matching '${birthLocation}'`);
-        return res.status(400).send('Invalid location.');
+    });
+
+    // Set up logout route
+    app.get('/logout', (req, res) => {
+      req.logout();
+      res.redirect('/');
+    });
+
+    // Set up game route
+    app.get('/game/blackjack', (req, res) => {
+      if (req.isAuthenticated()) {
+        res.sendFile(__dirname + '/public/game/index.html');
+        console.log("AUTHENTICATED!")
+      }
+      else {
+        res.redirect('/');
       }
 
-      const latitude = parseFloat(data[0].lat);
-      const longitude = parseFloat(data[0].lon);
+    });
 
-      const ephemerisData = await ephemeris.getAllPlanets(birthDateTime, longitude, latitude, 0);
+    app.get('/api/player', async (req, res) => {
+      const player = req.player;
+      // const player = await playerCollection.findOne({ discordUserId: discordPlayerData.id });
+      console.log("STILL SERVER ", player)
+      res.json({ player });
+    });
 
-      const origin = new pkg.Origin({
-          year: birthDateTime.getUTCFullYear(),
-          month: birthDateTime.getUTCMonth(),
-          date: birthDateTime.getUTCDate(),
-          hour: birthDateTime.getUTCHours(),
-          minute: birthDateTime.getUTCMinutes(),
-          latitude,
-          longitude,
-      });
-
-      const horoscope = new pkg.Horoscope({
-          origin,
-          houseSystem: "whole-sign",
-          zodiac: "tropical",
-          aspectPoints: ['bodies', 'points', 'angles'],
-          aspectWithPoints: ['bodies', 'points', 'angles'],
-          aspectTypes: ["major", "minor"],
-          customOrbs: {},
-          language: 'en'
-      });
-
-      const zodiacSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
-      const sunSign = zodiacSigns[Math.floor(ephemerisData.observed.sun.apparentLongitudeDd / 30)];
-      const moonSign = zodiacSigns[Math.floor(ephemerisData.observed.moon.apparentLongitudeDd / 30)];
-      const ascendantSign = horoscope._ascendant.Sign.label;
-      const midheavenSign = horoscope._midheaven.Sign.label;
-
-      const result = { sunSign, moonSign, ascendantSign, midheavenSign };
-      const chartData = { horoscope };
-      res.json({ result });
-  } catch (error) {
+  // API endpoint to update player record
+  app.post('/update-player-record', async (req, res) => {
+    try {
+      // Get data from request body
+      const { discordUserId, birthDate, birthTime, birthLocation, astroData } = req.body;
+  
+      // Check if the player record exists
+      const player = await playerCollection.findOne({ discordUserId });
+  
+      if (!player) {
+        console.log('Player record not found');
+        return res.status(404).json({ message: 'Player record not found' });
+      }
+  
+      // Update the player record in MongoDB
+      await playerCollection.updateOne(
+        { discordUserId },
+        { $set: {
+          birthDate,
+          birthTime,
+          birthLocation,
+          sunSign: astroData.sunSign,
+          moonSign: astroData.moonSign,
+          ascendantSign: astroData.ascendantSign,
+          midheavenSign: astroData.midheavenSign
+        }}
+      );
+  
+      // Send a success response
+      res.json({ message: 'Player record updated successfully!' });
+    } catch (error) {
+      // Log the error and send an error response
       console.error(error);
-      res.status(400).send('An error occurred. Please check your input and try again.');
-  }
-});
+      res.status(500).json({ message: 'An error occurred' });
+      }
+    });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+    app.get('/get-discord-user-id', (req, res) => {
+      if (req.session.discordUserId) {
+        console.log('ID IS ', req.session.discordUserId)
+        res.json({ discordUserId: req.session.discordUserId });
+      } else {
+        res.status(404).json({ error: 'Not found' });
+      }
+    });
+
+    // Start server
+    server.listen(process.env.PORT || 4000, () => {
+      console.log(`Server started on port ${process.env.PORT || 4000}!`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1);
+  });
